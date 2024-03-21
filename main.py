d8b07ee30891eeb8397705ff77fac12ef303ddc2
jakubpasieka <ins.art.pl@gmail.com>
2024-03-21 11:41:49 +0100

    Dodanie walidaci liter i znaków specjalnych.

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+def get_valid_packages_input(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value <= 0:
+                print("Wprowadzono niedozwolony znak/wartość, spróbuj jeszcze raz.")
+            else:
+                return value
+        except ValueError:
+            print("Wprowadzono niedozwolony znak/wartość, spróbuj jeszcze raz.")
+
+def get_valid_weight_input(prompt):
+    while True:
+        try:
+            weight = int(input(prompt))
+            if weight < 1 or weight > 10:
+                print("Podano nieprawidłową wagę. Koniec dodawania paczek, dotychczasowe produkty zostaną wysłane.")
+                return None
+            else:
+                return weight
+        except ValueError:
+            print("Wprowadzono niedozwolony znak, spróbuj jeszcze raz.")
+
 def main():
-    num_packages = int(input("Ile produktów chcesz wysłać? "))
+    num_packages = get_valid_packages_input("Ile produktów chcesz wysłać? ")
     packages = []
     current_package = []
 
     for i in range(num_packages):
-        weight = int(input(f"Podaj wagę produktu {i + 1} [1-10 kg]: "))
+        weight = get_valid_weight_input(f"Podaj wagę produktu {i + 1} [1-10 kg]: ")
+
+        if weight is None:
+            break
 
         if sum(current_package) + weight > 20:
             packages.append(current_package[:])
             current_package.clear()
-
-        if weight < 1 or weight > 10:
-            print("Podano nieprawidłową wagę. Koniec dodawania paczek, dotychczasowe produkty zostaną wysłane.")
-            break
 
         current_package.append(weight)
 
@@ -35,7 +57,6 @@ def main():
     print(f"Suma niewykorzystanych łącznie kilogramów: {unused_weight}kg")
     print(f"Najwięcej niewykorzystanych kilogramów ma paczka {packages.index(max_unused_package) + 1} "
           f"({max_unused_weight}kg)")
-
 
 if __name__ == "__main__":
     main()
